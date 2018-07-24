be5944ff1545f899bd7f364fb5ed0d86d47e9a25
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2018-07-24 18:32:00 +0200

    Minor test

--- app/src/test/java/de/fohgel/cartoonfisch/ExampleUnitTest.java
+++ app/src/test/java/de/fohgel/cartoonfisch/ExampleUnitTest.java
@@ -10,8 +10,14 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+	@Test
+	public void addition_isCorrect() {
+		assertEquals(4, 2 + 2);
+	}
+
+	@Test
+	public void addition_isWrong() {
+		assertNotEquals(5, 2 + 2);
+	}
+
 }
