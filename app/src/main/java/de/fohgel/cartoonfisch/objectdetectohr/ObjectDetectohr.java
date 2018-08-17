a3460f8f24bc21a16a6a6b168c929dab7aadfc26
Raphael Bialon <bialon@cs.uni-duesseldorf.de>
2018-08-17 18:15:51 +0200

    Add Tensorflow graph and labels file

--- app/src/main/java/de/fohgel/cartoonfisch/objectdetectohr/ObjectDetectohr.java
+++ app/src/main/java/de/fohgel/cartoonfisch/objectdetectohr/ObjectDetectohr.java
@@ -1,4 +1,23 @@
 package de.fohgel.cartoonfisch.objectdetectohr;
 
-public class ObjectDetectohr {
+import android.app.Activity;
+
+import java.io.IOException;
+
+public class ObjectDetectohr extends Activity {
+
+    private static final String TF_GRAPH = "file:///android_asset/frozen_interference_graph.pb";
+    private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/label_mapping.txt";
+    private static final int TF_OD_API_INPUT_SIZE = 300;
+
+    private Classifier detector;
+
+    public ObjectDetectohr() {
+        try {
+            detector = TensorFlowObjectDetectionAPIModel.create(
+                    getAssets(), TF_GRAPH, TF_OD_API_LABELS_FILE, TF_OD_API_INPUT_SIZE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
